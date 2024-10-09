6f542f54db9774d23f0fcfebebc84b7808b4ab59
JanMurillo83 <antonio.nieto@nietoconsulting.com.mx>
2024-10-09 16:11:36 -0600

    091024-2

--- routes/web.php
+++ routes/web.php
@@ -1,2 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EmpleadosController;
+
+Route::group(['middleware' => ['web']], function () {
+    //Route::get('/', function () { return view('views/login');});
+    Route::get('/', function () { return redirect('main');});
+});

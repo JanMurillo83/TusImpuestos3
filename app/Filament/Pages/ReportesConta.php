e1f2a66bd74a15ce4d677ac6542883e8d654cfd5
JanMurillo83 <antonio.nieto@nietoconsulting.com.mx>
2025-05-02 10:39:01 -0600

    020525-1

--- app/Filament/Pages/ReportesConta.php
+++ app/Filament/Pages/ReportesConta.php
@@ -4,12 +4,18 @@ namespace App\Filament\Pages;
 
 use App\Http\Controllers\ReportesController;
 use Filament\Facades\Filament;
+use Filament\Forms\Components\Actions;
+use Filament\Forms\Components\Fieldset;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Forms\Form;
 use Filament\Pages\Page;
 use Filament\Actions\Action;
 use Torgodly\Html2Media\Actions\Html2MediaAction;
 
-class ReportesConta extends Page
+class ReportesConta extends Page implements HasForms
 {
+    Use InteractsWithForms;
     protected static ?string $navigationIcon = 'fas-print';
     //protected static ?string $cluster = ContReportes::class;
     //protected static bool $isScopedToTenant = false;
@@ -17,32 +23,43 @@ class ReportesConta extends Page
     protected static ?string $title = 'Reportes';
     protected static ?string $pluralLabel = 'Reportes';
     protected static string $view = 'filament.pages.reportes-conta';
+
+    public function form(Form $form): Form{
+        return $form
+            ->schema([
+                Fieldset::make('Reportes PDF')
+                ->schema([
+                Actions::make([
+                  Actions\Action::make('Balance_General')
+                      ->action(function (){
+                          (new \App\Http\Controllers\ReportesController)->actualiza_saldos(Filament::getTenant()->periodo,Filament::getTenant()->ejercicio);
+                          $this->getAction('Balance General')->visible(true);
+                          $this->replaceMountedAction('Balance General');
+                          $this->getAction('Balance General')->visible(false);
+                      }),
+                    Actions\Action::make('Balanza_General')
+                        ->label('Balanza de Comprobacion')
+                        ->action(function (){
+                            (new \App\Http\Controllers\ReportesController)->actualiza_saldos(Filament::getTenant()->periodo,Filament::getTenant()->ejercicio);
+                            $this->getAction('Balanza General')->visible(true);
+                            $this->replaceMountedAction('Balanza General');
+                            $this->getAction('Balanza General')->visible(false);
+                        }),
+                    Actions\Action::make('EdoRes')
+                        ->label('Estado de Resultados')
+                        ->action(function (){
+                            (new \App\Http\Controllers\ReportesController)->actualiza_saldos(Filament::getTenant()->periodo,Filament::getTenant()->ejercicio);
+                            $this->getAction('Estado de Resultados')->visible(true);
+                            $this->replaceMountedAction('Estado de Resultados');
+                            $this->getAction('Estado de Resultados')->visible(false);
+                        })
+                ])
+                ])
+            ]);
+    }
     protected function getActions(): array
     {
         return [
-            Action::make('Balance_General')
-            ->action(function (){
-                (new \App\Http\Controllers\ReportesController)->actualiza_saldos(Filament::getTenant()->periodo,Filament::getTenant()->ejercicio);
-                $this->getAction('Balance General')->visible(true);
-                $this->replaceMountedAction('Balance General');
-                $this->getAction('Balance General')->visible(false);
-            }),
-            Action::make('Balanza_General')
-                ->label('Balanza de Comprobacion')
-                ->action(function (){
-                    (new \App\Http\Controllers\ReportesController)->actualiza_saldos(Filament::getTenant()->periodo,Filament::getTenant()->ejercicio);
-                    $this->getAction('Balanza General')->visible(true);
-                    $this->replaceMountedAction('Balanza General');
-                    $this->getAction('Balanza General')->visible(false);
-                }),
-            Action::make('EdoRes')
-                ->label('Estado de Resultados')
-                ->action(function (){
-                    (new \App\Http\Controllers\ReportesController)->actualiza_saldos(Filament::getTenant()->periodo,Filament::getTenant()->ejercicio);
-                    $this->getAction('Estado de Resultados')->visible(true);
-                    $this->replaceMountedAction('Estado de Resultados');
-                    $this->getAction('Estado de Resultados')->visible(false);
-                }),
             Html2MediaAction::make('Balance General')
                 ->preview()
                 ->print(false)
@@ -50,8 +67,9 @@ class ReportesConta extends Page
                 ->filename('Balance General')
                 ->margin([10, 10, 10, 10])
                 ->content(fn()=>
-                    view('BalanceGral',['empresa'=>Filament::getTenant()->id,'periodo'=>Filament::getTenant()->periodo,'ejercicio'=>Filament::getTenant()->ejercicio])
-                )->visible(false),
+                    view('BGralNew',['empresa'=>Filament::getTenant()->id,'periodo'=>Filament::getTenant()->periodo,'ejercicio'=>Filament::getTenant()->ejercicio])
+                )->visible(false)
+                ->modalWidth('7xl'),
             Html2MediaAction::make('Balanza General')
                 ->preview()
                 ->print(false)
@@ -59,8 +77,9 @@ class ReportesConta extends Page
                 ->filename('Balanza de Comprobacion')
                 ->margin([10, 10, 10, 10])
                 ->content(fn()=>
-                view('BalanzaCompro',['empresa'=>Filament::getTenant()->id,'periodo'=>Filament::getTenant()->periodo,'ejercicio'=>Filament::getTenant()->ejercicio])
-                )->visible(false),
+                view('AGralNew',['empresa'=>Filament::getTenant()->id,'periodo'=>Filament::getTenant()->periodo,'ejercicio'=>Filament::getTenant()->ejercicio])
+                )->visible(false)
+                ->modalWidth('7xl'),
             Html2MediaAction::make('Estado de Resultados')
                 ->preview()
                 ->print(false)
@@ -68,7 +87,7 @@ class ReportesConta extends Page
                 ->filename('Estado de Resultados')
                 ->margin([10, 10, 10, 10])
                 ->content(fn()=>
-                view('EdoRes',['empresa'=>Filament::getTenant()->id,'periodo'=>Filament::getTenant()->periodo,'ejercicio'=>Filament::getTenant()->ejercicio])
+                view('EdoreNew',['empresa'=>Filament::getTenant()->id,'periodo'=>Filament::getTenant()->periodo,'ejercicio'=>Filament::getTenant()->ejercicio])
                 )->visible(false)
         ];
     }

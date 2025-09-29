9c8a7a081cc38dc358e2aa1cedf16dd3189ea9fe
JanMurillo83 <antonio.nieto@nietoconsulting.com.mx>
2025-09-29 07:33:48 -0600

    290925-C2

--- database/migrations/2025_09_26_140021_add_pendiente_factura.php
+++ database/migrations/2025_09_26_140021_add_pendiente_factura.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        DB::statement('ALTER TABLE facturas DROP COLUMN pendiente_pago');
+        //DB::statement('ALTER TABLE facturas DROP COLUMN pendiente_pago');
         Schema::table('facturas', function (Blueprint $table) {
             $table->decimal('pendiente_pago',18,8)->after('xml_cancela')->default(0);
         });

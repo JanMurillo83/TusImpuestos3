9d64f3b7361c2a6a589a64c1165bd1da49933b21
JanMurillo83 <antonio.nieto@nietoconsulting.com.mx>
2025-11-23 17:01:12 -0600

    231125-2

--- database/migrations/2025_11_23_130842_alta_proveedores.php
+++ database/migrations/2025_11_23_130842_alta_proveedores.php
@@ -12,21 +12,24 @@ return new class extends Migration
      */
     public function up(): void
     {
-        $cfdis = \App\Models\Almacencfdis::where('xml_type','Recibidos')->get();
-        foreach($cfdis as $cfdi){
-            if(!DB::table('proveedores')->where('team_id',$cfdi->team_id)->where('rfc',$cfdi->Emisor_Rfc)->exists())
-            {
-                $clave = count(DB::table('proveedores')->where('team_id',$cfdi->team_id)->get()) + 1;
-                \App\Models\Proveedores::create([
-                    'clave'=>$clave,
-                    'rfc'=>$cfdi->Emisor_Rfc,
-                    'nombre'=>$cfdi->Emisor_Nombre,
-                    'team_id'=>$cfdi->team_id,
-                    'dias_credito'=>30
-                ]);
+        try {
+            $cfdis = \App\Models\Almacencfdis::where('xml_type', 'Recibidos')->where('TipoDeComprobante', 'I')->get();
+            foreach ($cfdis as $cfdi) {
+                if (!DB::table('proveedores')->where('team_id', $cfdi->team_id)->where('rfc', $cfdi->Emisor_Rfc)->exists()) {
+                    $clave = count(DB::table('proveedores')->where('team_id', $cfdi->team_id)->get()) + 1;
+                    \App\Models\Proveedores::create([
+                        'clave' => $clave,
+                        'rfc' => $cfdi->Emisor_Rfc,
+                        'nombre' => $cfdi->Emisor_Nombre,
+                        'team_id' => $cfdi->team_id,
+                        'dias_credito' => 30
+                    ]);
+                }
             }
+            \Illuminate\Support\Facades\DB::statement('UPDATE proveedores SET dias_credito = 30 WHERE id > 0');
+        }catch(Exception $e){
+            error_log($e->getMessage());
         }
-        \Illuminate\Support\Facades\DB::statement('UPDATE proveedores SET dias_credito = 30 WHERE id > 0');
     }
 
     /**

aaad683d53b3dd433c7a539307105d3a04f23d5a
JanMurillo83 <antonio.nieto@nietoconsulting.com.mx>
2025-12-04 09:10:53 -0600

    041225-6

--- app/Filament/Resources/CatPolizasResource.php
+++ app/Filament/Resources/CatPolizasResource.php
@@ -116,18 +116,7 @@ class CatPolizasResource extends Resource
                 Forms\Components\TextInput::make('concepto')
                     ->required()
                     ->maxLength(255)
-                    ->columnSpan(4)
-                    ->formatStateUsing(function ($state) {
-                        $truncatedValue = Str::limit($state, 50);
-                        return new HtmlString("<span title='{$state}'>{$truncatedValue}</span>");
-                    })
-                    ->action(Action::make('Concepto')->form([
-                        TextInput::make('Concepto')
-                            ->hiddenLabel()->readOnly()
-                            ->default(function($record){
-                                return $record->concepto;
-                            })
-                    ])),
+                    ->columnSpan(4),
                 Forms\Components\TextInput::make('referencia')
                     ->maxLength(255)
                     ->prefix('F-'),
@@ -275,7 +264,18 @@ class CatPolizasResource extends Resource
                     ->sortable(),
 
                 Tables\Columns\TextColumn::make('concepto')
-                    ->searchable(),
+                    ->searchable()
+                    ->formatStateUsing(function ($state) {
+                        $truncatedValue = Str::limit($state, 50);
+                        return new HtmlString("<span title='{$state}'>{$truncatedValue}</span>");
+                    })
+                    ->action(Action::make('Concepto')->form([
+                        TextInput::make('Concepto')
+                            ->hiddenLabel()->readOnly()
+                            ->default(function($record){
+                                return $record->concepto;
+                            })
+                    ])),
                 Tables\Columns\TextColumn::make('referencia')
                     ->searchable()
                     ->prefix('F-'),
